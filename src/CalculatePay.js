8733a0f164d86a110729bd3f14e1a853a3bd70fb
Ananda <anrodziewiczz@gmail.com>
2021-07-04 20:41:37 +1000

    added functionality to calculatepay class

--- src/CalculatePay.js
+++ src/CalculatePay.js
@@ -1,5 +1,36 @@
 const { User } = require("./User");
 class CalculatePay extends User{
+    constructor(name, salary) {
+        this.name = name
+        this.salary = salary
+    }
+
+    getInfo() {
+        this.getName()
+        this.getSalary()
+    }
+
+    getName() {
+        let name = prompt("What is your name? ")
+
+        if (name === "" || name == false) {
+            console.log("Enter a valid name")
+            return this.getName()
+        }
+        return this.name = name 
+    }
+
+    getSalary() {
+        let response = prompt("What is your annual salary? ")
+        let salary = parseInt(response)
+        
+        if (salary < 0 || !(salary > 0)) {
+            console.log("Enter a valid salary")
+            return this.getSalary()
+        }
+        return this.salary = salary
+    }
+    
 }
 module.exports = {
   CalculatePay,
