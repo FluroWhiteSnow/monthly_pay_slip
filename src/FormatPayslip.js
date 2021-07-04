eb279b383bd6c92cef258a02234f06bde3ce4a1f
Ananda <anrodziewiczz@gmail.com>
2021-07-04 21:55:59 +1000

    added logic to formatpayslip

--- src/FormatPayslip.js
+++ src/FormatPayslip.js
@@ -1,6 +1,24 @@
 const { CalculatePay } = require("./CalculatePay");
 
 class FormatPayslip extends CalculatePay {
+    constructor(name, salary, grossMonthly, monthlyTax, netMonthly, ) {
+        super(name, grossMonthly, monthlyTax, netMonthly, salary)
+    }
+
+    runFormat() {
+        this.getInfo()
+        this.runCalc()
+        this.makePayslip()
+    }
+
+    makePayslip() {
+       console.log(`
+        Monthly Payslip for:  ${this.name}
+        Gross Monthly Income: ${this.grossMonthly}
+        Monthly Income Tax:   ${this.monthlyTax}
+        Net Monthly Income:   ${this.netMonthly}
+        `)
+    } 
 }
 
 module.exports = {
